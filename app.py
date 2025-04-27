3a4d2174ae002703adec2c12867ff06330f782c7
CB230494 <161616911+CB230494@users.noreply.github.com>
2025-04-27 11:11:07 -0600

    app.py

--- app.py
+++ app.py
@@ -181,12 +181,14 @@ def generar_pdf(datos):
                 "socioculturales y estructurales, así como en las problemáticas priorizadas, fomentando también la correcta documentación "
                 "de balances operativos e informes de gestión, en el marco de la Estrategia Integral Sembremos Seguridad.")
 
-    add_section("Antecedentes como Referencia para el Taller",
-                "Durante la revisión de las órdenes de ejecución previas, se identificaron los siguientes hallazgos:")
+    
 
     pdf.add_page()
 
     # ---- Página 2 y 3 ----
+    add_section("Antecedentes como Referencia para el Taller",
+                "Durante la revisión de las órdenes de ejecución previas, se identificaron los siguientes hallazgos:")
+    
     add_table("Antecedentes como Referencia para el Taller", datos["antecedentes"])
     add_table("Evaluación de la Aplicación de Insumos Mostrados en el Taller", datos["insumos"])
     add_table("Evaluación de la Elaboración de la Orden de Ejecución durante el Taller", datos["orden"])
